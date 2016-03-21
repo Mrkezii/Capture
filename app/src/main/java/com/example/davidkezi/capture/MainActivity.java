c5a5f1f39b96c71ddfb7eb2233ebefa4eebee6bd
mrkezii <david.kezi@yahoo.com>
2016-03-21 09:49:35 +0300

    Created design and method to get file in activity + manifest

--- app/src/main/java/com/example/davidkezi/capture/MainActivity.java
+++ app/src/main/java/com/example/davidkezi/capture/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.davidkezi.capture;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
+public class MainActivity extends Activity {
+    Button btn;
+    ImageView imageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        imageview = (ImageView) findViewById(R.id.imageView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private File getFile()
+    {
+        File folder = new File("sdcard/camera_app");
+        if(!folder.exists())
+        {
+            folder.mkdir();
+        }
+        File image_file = new File(folder, "camera_image.jpg");
+        return image_file;
+
     }
 }
